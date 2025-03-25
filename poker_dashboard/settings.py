1fc78ccae004e67bac67bfea17890052071b5961
Lucas Menezes <lu289997@gmail.com>
2025-03-25 20:58:47 -0300

    Corrigindo configuração de deploy

--- poker_dashboard/settings.py
+++ poker_dashboard/settings.py
@@ -24,6 +24,20 @@ INSTALLED_APPS = [
     'results',
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
