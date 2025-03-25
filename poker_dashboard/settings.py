70a7718d68b23a9c87b2b15c6e0160aedac28f86
Lucas Menezes <lu289997@gmail.com>
2025-03-25 16:40:39 -0300

    Configurações para produção com Gunicorn e Waitress

--- poker_dashboard/settings.py
+++ poker_dashboard/settings.py
@@ -110,6 +110,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
